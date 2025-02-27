e08c084329c693edb70200e70a02f5a8292caade
alfonsekon <agdelosreyes1@up.edu.ph>
2025-02-28 00:12:51 +0800

    added android-studio to sessionizer

--- scripts/wezterm_sessionizer.sh
+++ scripts/wezterm_sessionizer.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-selected=$(find . -mindepth 1 -maxdepth 1 -type d | fzf)
+CURRENT_PATH="$PWD"
+
+selected=$(find "$CURRENT_PATH" -mindepth 1 -maxdepth 1 -type d | fzf)
 
 [ -z "$selected" ] && exit
 
 selected=$(realpath "$selected")
 
-code -n --reuse-window "$selected"
+if [[ "$selected" == *"android-studio/"* ]]; then
+    echo "Opening in android-studio..."
+    android-studio "$selected" &
+else
+    echo "Opening in vscode..."
+    code -n --reuse-window "$selected" &
+fi
