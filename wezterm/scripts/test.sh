b753b49a7d119410ce796428e4680164a9f991a7
alfonsekon <agdelosreyes1@up.edu.ph>
2025-03-06 20:55:21 +0800

    idk whats happening here

--- wezterm/scripts/test.sh
+++ wezterm/scripts/test.sh
@@ -1,5 +1,16 @@
 # #!/bin/bash
 
+
+#
+#
+#
+# i do not know what this file is for anymore
+#
+#
+#
+
+
+
 # project=$(find ~/ ~/coding/android-studio/ ~/coding/vscode ~/school/third-year/second-sem ~/dotfiles -mindepth 1 -maxdepth 1 | fzf)
 
 # if [[ -z "$project" ]]; then
@@ -23,17 +34,49 @@
 #only call/execute sessionizers within a bash session.
 #this will not behave as expected when inside vim or other TUI applications
 
-selected=$(find ~/ ~/coding/android-studio/ ~/coding/vscode ~/school/third-year/second-sem ~/dotfiles -mindepth 1 -maxdepth 1 | fzf)
+#REEEEEEEEEEEEEEEe
+#!/bin/bash
+
+#only call/execute sessionizers within a bash session.
+#this will not behave as expected when inside vim or other TUI applications
+
+selected=$(find $(pwd) ~/ ~/coding/android-studio/ ~/coding/vscode ~/school/third-year/second-sem ~/dotfiles -mindepth 1 -maxdepth 1 | fzf)
 
 if [[ -z "$selected" ]]; then
+    wezterm cli spawn --cwd "$PWD"
     exit 0
 fi
 
 selected=$(realpath "$selected")
 
-number=$(wezterm cli spawn --cwd "$selected")
-wezterm cli set-tab-title --tab-id "$number" "$(basename "$selected")"
+if [[ "$selected" == *"android-studio/"* ]]; then
+    #won't add the check if it's a dir or not since ill prolly open full projects with android studio anyways
+    original_tab=$()
+    tab_id=$(wezterm cli spawn --cwd "$selected")
+    wezterm cli set-tab-title --pane-id "$tab_id" "$(basename "$selected")"
 
-sleep 0.1
+    echo "Opening in android-studio..."
+    android-studio "$selected" > /dev/null 2>&1 
+    wezterm cli kill-pane
 
-nvim "$selected"
+    i3 workspace 3
+else
+    if [[ -d "$selected" ]]; then
+        cd "$selected"
+        wezterm cli set-tab-title "$(basename "$selected")"
+
+        echo "Opening in vscode..."
+        code -n --reuse-window "$selected" 
+        i3 workspace 3
+    else
+        dir_name=$(dirname "$selected")
+        cd "$dir_name"
+
+        wezterm cli set-tab-title "$(basename "$dir_name")"
+        echo "Opening in vscode..."
+        code -n --reuse-window "$selected" 
+        i3 workspace 3
+    fi
+fi
+
+clear
