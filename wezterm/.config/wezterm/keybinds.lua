a46f227f3cead501427fef223f08c9f9b9243cbc
alfonsekon <agdelosreyes1@up.edu.ph>
2025-03-12 22:35:39 +0800

    keybind change

--- wezterm/.config/wezterm/keybinds.lua
+++ wezterm/.config/wezterm/keybinds.lua
@@ -110,7 +110,7 @@ keys.binds = {
 	--STUFF WITH LEADER
 	{ key = "a", mods = "LEADER|CTRL", action = wezterm.action.SendKey({ key = "a", mods = "CTRL" }) },
 	{ key = "c", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
-	{ key = "p", mods = "LEADER", action = act.ActivateTabRelativeNoWrap(-1) },
+	{ key = "b", mods = "LEADER", action = act.ActivateTabRelativeNoWrap(-1) },
 	{ key = "n", mods = "LEADER", action = act.ActivateTabRelativeNoWrap(1) },
 	{ key = "1", mods = "LEADER", action = act.ActivateTab(0) },
 	{ key = "2", mods = "LEADER", action = act.ActivateTab(1) },
